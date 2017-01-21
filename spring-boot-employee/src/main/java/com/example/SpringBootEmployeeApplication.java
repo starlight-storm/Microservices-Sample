1c6c2b9f3ad510776428ee8aa7ce49980f8a929b
Hasegawa Yuichi <hase@starlight-storm.com>
2017-01-21 14:38:48 +0900

    名前をプロジェクト名に合わせる.

--- spring-boot-employee/src/main/java/com/example/SpringBootEmployeeApplication.java
+++ spring-boot-employee/src/main/java/com/example/SpringBootEmployeeApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TrainingApplication {
+public class SpringBootEmployeeApplication {
 	
 	public static void main(String[] args) {
-		SpringApplication.run(TrainingApplication.class, args);
+		SpringApplication.run(SpringBootEmployeeApplication.class, args);
 	}
 }
--- spring-boot-employee/src/test/java/com/example/SpringBootEmployeeApplicationTests.java
+++ spring-boot-employee/src/test/java/com/example/SpringBootEmployeeApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TrainingApplicationTests {
+public class SpringBootEmployeeApplicationTests {
 
 	@Test
 	public void contextLoads() {
