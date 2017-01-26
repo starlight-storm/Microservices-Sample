105d6bc22842208dc47b35df13938fbd35ae0c33
Hasegawa Yuichi <hase@starlight-storm.com>
2017-01-26 11:32:22 +0900

    second commit.

--- piko/src/main/java/com/example/api/v1/PikoRestController.java
+++ piko/src/main/java/com/example/api/v1/PikoRestController.java
@@ -4,6 +4,7 @@ import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 public class PikoRestController {
 
 	@Autowired
+	@LoadBalanced
     RestTemplate restTemplate;
 	
     @Value("${apple.api:http://localhost:3333}")
